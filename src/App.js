d4bc1eae9f33d15851da20753176dceaa69d4f7d
ananth488 <94784374+ananth488@users.noreply.github.com>
2023-05-10 14:52:55 +0530

    first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import Api from './component/Api';
 
 function App() {
+
+
+  const [apiData, setapiDtate] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(data => data.json())
+      .then(data => {
+        setapiDtate(data)
+      });
+  }, [])
+
+
+
+  // const[api,setapi]=useState([])
+
+  //   useEffect(()=>{
+  //                     fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+  //                     .then(data=>{return data.json()})
+  //                     // .then(user=>{console.log(user)})
+  //                     .then(data=>{
+
+  //                         setapi(data);
+  //                      console.log(data)
+  //                       // console.log(userDetails);
+
+  //                     })
+  //                     // .catch(error=>{
+  //                     //   return alert(error);
+  //                     // })
+  //                 },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+
+
+        {/* {
+      api.map((user)=>{
+              return(
+         
+               <p>{user.name}</p>
+               )
+      })
+    } */}
+
+        {
+          apiData ?
+            (
+              apiData.map((pos) => {
+                return (
+                  <>
+                    <div className='container'>
+                      <div className='row'>
+                        <div className='col'>
+                          <div class="card mt-3" >
+                            <div class="card-body ">
+                              <h1>{pos.id}</h1>
+                              <h5 class="card-title">{pos.title}</h5>
+                              <p class="card-text">{pos.body}</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div className='col'>
+                          <div class="card mt-3" >
+                            <div class="card-body ">
+                              <h1>{pos.id}</h1>
+                              <h5 class="card-title">{pos.title}</h5>
+                              <p class="card-text">{pos.body}</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                        </div>
+                        <div className='col'>
+                          <div class="card mt-3" >
+                            <div class="card-body ">
+                              <h1>{pos.id}</h1>
+                              <h5 class="card-title">{pos.title}</h5>
+                              <p class="card-text">{pos.body}</p>
+                              <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  </>
+                )
+              }
+              )
+            )
+            :
+            (<h3>Loading...</h3>)
+        }
+      </div>
+
+
+
+    </>
+
   );
 }
 
---
+++ src/component/Api.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState,useEffect} from 'react'
+const Api = () => {
+
+    
+  return (
+    <>
+    <h1>API</h1>
+
+   
+    </>
+  )
+}
+
+export default Api
